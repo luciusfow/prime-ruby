fac16a81fbdcdae3375ef1113d696c1ab2ad704a
Lucius Fowler <luciusfow@gmail.com>
2020-12-19 17:16:52 -0500

    commit tree 8ee02c66c736edaa9a0f3e1b02ec4dd35cea76dd

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+require "prime"
+def prime?(num)
+    num.prime? ? true : false
+    # if num < 0 || num == 0 || num == 1
+    #     return false
+    #     (2..num -1).to_a.all? |pos_num|
+    #     num % pos_num ≠ 0
+    # else
+    # end
+    # end
+end
+# def prime?(num)
+#     if num > 1
+#         # while > 1 
+#             n = 2
+#             if num % == 0
+#                 return false
+#                 n += 1
+#             else 
+#                 return true
+#             end
+#         end
+#     end
+# end
